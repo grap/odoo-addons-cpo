9500a90c9ff4422cb2392583354cb7a263d327b6
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-10-10 17:59:56 +0200

    [FIX] 'purchase_compute_order' Flake8;

--- purchase_compute_order_pos/__openerp__.py
+++ purchase_compute_order_pos/__openerp__.py
@@ -1,12 +1,35 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Purchase - Computed Purchase Order Glue Module for Point Of Sale for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Julien WESTE
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'Computed Purchase Order - POS',
+    'name': 'Computed Purchase Order +Point of Sale (Glue Module)',
     'version': '0.1',
     'category': 'Purchase',
     'description': """
-Glue module that allow to include draft pos_orders in the purchase_compute_order calculation.
-=======================================================
+Glue module that allow to include draft pos_orders in
+the purchase_compute_order calculation.
+=====================================================
+=======================================
 
 Contacts :
 ----------
@@ -21,10 +44,5 @@ Contacts :
         'purchase_compute_order',
         'point_of_sale',
     ],
-    'init_xml': [],
-    'demo_xml': [],
-    'update_xml': [],
-    'application' : True,
-    'installable': True,
     'auto_install': True,
 }
--- purchase_compute_order_pos/model/product_product.py
+++ purchase_compute_order_pos/model/product_product.py
@@ -1,27 +1,49 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See __openerp__.py file for Copyright and Licence Informations.
-################################################################################
+##############################################################################
+#
+#    Purchase - Computed Purchase Order Glue Module for Point Of Sale for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Julien WESTE
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from openerp.osv.orm import Model
-from openerp.osv import fields
+
 
 class product_product(Model):
     _inherit = 'product.product'
 
     ### Private section
-    def _get_draft_outgoing_qty(self, cr, uid, ids, fields, arg, context=None):
+    def _get_draft_outgoing_qty(
+            self, cr, uid, ids, fields, arg, context=None):
         """ empty function.
             Please Overload this function, in glue module.
         """
-        res = super(product_product, self)._get_draft_outgoing_qty(cr, uid, ids, fields, arg, context=context)
+        res = super(product_product, self)._get_draft_outgoing_qty(
+            cr, uid, ids, fields, arg, context=context)
         pol_obj = self.pool.get('pos.order.line')
-        pol_ids = pol_obj.search(cr, uid, [('state','=','draft'), ('product_id','in',ids)], context=context)
+        pol_ids = pol_obj.search(cr, uid, [
+            ('state', '=', 'draft'),
+            ('product_id', 'in', ids)], context=context)
         draft_qty = {}
 
         for line in pol_obj.browse(cr, uid, pol_ids, context=context):
             draft_qty.setdefault(line.product_id.id, 0)
-            draft_qty[line.product_id.id] -= line.qty 
+            draft_qty[line.product_id.id] -= line.qty
         for pp in self.browse(cr, uid, ids, context=context):
             res.setdefault(pp.id, 0)
             res[pp.id] += draft_qty.get(pp.id, 0)
