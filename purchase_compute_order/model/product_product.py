d67989e601b6e6fdc64b1e492436fe7b1398d027
Alvaro Marcos <alvaro.marcos@factorlibre.com>
2018-12-27 12:57:26 +0100

    Gestión de proveedor principal y cálculo precios

--- purchase_compute_order/model/product_product.py
+++ purchase_compute_order/model/product_product.py
@@ -24,6 +24,22 @@
 from openerp import models, fields, api
 
 
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    principal_supplier = fields.Many2one('product.supplierinfo',
+                                         'Principal Supplier',
+                                         compute='_calc_principal_supplier')
+
+    @api.multi
+    def _calc_principal_supplier(self):
+        # import pdb; pdb.set_trace()
+        # seller_ids.orderby('secuency')
+        for prod in self:
+            prod.principal_supplier = self.env['product.supplierinfo'].search([
+                ('id', 'in', prod.seller_ids.ids)], order="sequence", limit=1)
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
