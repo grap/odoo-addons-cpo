8664416b76c44b2f87016ab3fa9bf70256b21f5a
rodrigo <rodrigo.bonilla@factorlibre.com>
2019-09-11 11:39:57 +0200

    se añade traduccion de campos

--- purchase_compute_order_product_filter_brand/models/__init__.py
+++ purchase_compute_order_product_filter_brand/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2016 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
+# © 2019 FactorLibre - Rodrigo Bonilla <rodrigo.bonilla@factorlibre.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import computed_purchase_order
