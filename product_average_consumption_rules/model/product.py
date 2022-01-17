e6e3c7f1ec68cf82b5f10a2167682f143b7dab15
Hugo Santos <hugo.santos@factorlibre.com>
2022-01-17 12:14:39 +0100

    Corregir dominios de ubicaciones y estados en base a config

--- product_average_consumption_rules/model/product.py
+++ product_average_consumption_rules/model/product.py
@@ -22,10 +22,10 @@ class ProductProduct(models.Model):
             })
         domain_products = [('product_id', 'in', product_ids)]
         domain_move_out = []
-        domain_quant_loc, domain_move_in_loc, domain_move_out_loc = \
-            self._get_domain_locations()
+        domain_location = self._get_average_consumption_location_domain()
+        domain_states = self._get_average_consumption_states_domain()
         domain_move_out += self._get_domain_dates(ctx) \
-            + [('state', 'in', ('confirmed', 'waiting', 'assigned', 'done'))] \
+            + domain_states \
             + domain_products
         rules = self.env['consumption.rules'].search([
             ('apply_to_calculation', '=', True)
@@ -36,7 +36,7 @@ class ProductProduct(models.Model):
                                 '&', ('date', '>=', rules.initial_date),
                                 ('date', '<', rules.end_date), '&']
             domain_move_out += alternate_domain
-        domain_move_out += domain_move_out_loc
+        domain_move_out += domain_location
         return domain_move_out, begin_date, ctx
 
     # Prototipo para calculo con fehcas mayores a 365
